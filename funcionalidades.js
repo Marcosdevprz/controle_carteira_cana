94734ac843ed355949b52bb24735d365322459b2
unknown <marcosdesenvolvedor5@gmail.com>
2025-07-01 21:22:16 -0300

    pfv vaaiiii

--- funcionalidades.js
+++ funcionalidades.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDuCRjLF1aNgfh0bPKiknm1HWUQ1dA6dmI",
     authDomain: "carteiracana.firebaseapp.com",
     projectId: "carteiracana",
-    // CORREÇÃO: Revertendo para o valor original fornecido pelo Firebase.
+    // CORREÇÃO FINAL: Revertendo para o valor original fornecido pelo Firebase.
     storageBucket: "carteiracana.firebasestorage.app",
     messagingSenderId: "950424552534",
     appId: "1:950424552534:web:b96c8a85ba02e4868127f7"
